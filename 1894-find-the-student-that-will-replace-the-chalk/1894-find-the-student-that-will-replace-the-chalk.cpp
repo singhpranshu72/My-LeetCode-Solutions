9ae2782b5b9fc7db33e1cb9a07b80497283999fc
Pranshu Singh <110157886+8vFail@users.noreply.github.com>
2022-10-11 18:28:07 +0530

    Time: 1139 ms (5.09%), Space: 74.5 MB (36.99%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -3,12 +3,12 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
         int n=chalk.size();
       long long int p=accumulate(chalk.begin(),chalk.end(),0l);
-        k=k%p;
-        // while(1){
-        //     if(k<p)break;
-        //     else
-        //         k=k-p;
-        // }
+       
+        while(1){
+            if(k<p)break;
+            else
+                k=k-p;
+        }
         
          for (int i=0;i<n;i++)
         if ((k -= chalk[i]) < 0)
