75035f3b425994183aa3ca8cd0f2b8994d7a17af
Pranshu Singh <110157886+8vFail@users.noreply.github.com>
2022-10-12 19:21:03 +0530

    Time: 4 ms (42.91%), Space: 7.6 MB (77.55%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int, vector<int>> q;
+        int x, y;
+      for(auto x:stones)
+            q.push(x);
+        
+        while(q.size() != 1){
+            y = q.top();
+            q.pop();
+            x = q.top();
+            q.pop();
+
+            q.push(y-x);
+        }
+        
+        return q.top();
+    }
+};
